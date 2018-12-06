4927cb7f0f734a05796eed80b50dba46be1c7723
namisumida <sumida.nami@gmail.com>
2018-12-05 21:54:24 -0500

    Adding paths

--- js/build.js
+++ js/build.js
@@ -1,56 +1,119 @@
 var w = 800;
 var h = 800;
+var dataset;
 
 // Colors
+var greenRainbow = new Rainbow();
+greenRainbow.setSpectrum('#510602', '#ff7a73');
+greenRainbow.setNumberRange(1, 15);
 
 // Create leaflet map
-var dcMap = L.map('map-container').setView([38.91, -77.04], 13);
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-            subdomains: 'abcd',
-            maxZoom: 19
-        }).addTo(dcMap);
+function addLmaps() {
+  dcMap = L.map('map-container').setView([38.91, -77.04], 13);
+  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+              subdomains: 'abcd',
+              maxZoom: 19
+          }).addTo(dcMap);
 
-// fit the SVG element to leaflet's map layer
-function reset() {
-  bounds = path.bounds(geoShape);
-  var topLeft = bounds[0]
-  var bottomRight = bounds[1];
-
-  svg.attr("width", bottomRight[0] - topLeft[0])
-     .attr("height", bottomRight[1] - topLeft[1])
-     .style("left", topLeft[0] + "px")
-     .style("top", topLeft[1] + "px");
-
-  g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-
-  // initialize the path data
-  d3_features.attr("d", path)
-             .style("fill-opacity", 0.7)
-             .attr('fill','blue');
+  L.svg({clickable:true}).addTo(dcMap);
 };
 // Use Leaflet to implement a D3 geometric transformation.
 function projectPoint(x, y) {
-  var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+  var point = dcMap.latLngToLayerPoint(new L.LatLng(y, x));
   this.stream.point(point.x, point.y);
 };
-
-// Add an SVG element to Leaflet’s overlay pane
-var svg = d3.select(dcMap.getPanes().overlayPane).append("svg");
-var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+var d3_features;
 
 
-d3.json("Data/Grocery_Store_Locations.geojson", function(geoShape) {
+d3.json("Data/Grocery_Store_Locations.json", function(geoShape) {
 
-	//  create a d3.geo.path to convert GeoJSON to SVG
-	var projection = d3.geo.transform({ point: projectPoint });
-  var path = d3.geo.path().projection(projection);
+  dataset = geoShape;
+  addLmaps();
 
-	// create path elements for each of the features
-	d3_features = g.selectAll("path")
-                	.data(geoShape.features)
-                	.enter()
-                  .append("path");
-	map.on("viewreset", reset);
+  // Draw features
+  var svg = d3.select("#map-container").select("svg")
+              .attr("pointer-events", "auto")
+  var g = svg.append("g");
+  var projection = d3.geoTransform({ point: projectPoint });
+  var path = d3.geoPath().projection(projection);
+  d3_features = g.selectAll("points")
+                    	.data(dataset.features)
+                    	.enter()
+                      .append("path")
+                      .style("fill", function(d) {
+                        if (d.properties.PRESENT90=="Yes") {
+                          return greenRainbow.colourAt(1)
+                        }
+                        else if (d.properties.PRESENT95=="Yes") {
+                          return greenRainbow.colourAt(2)
+                        }
+                        else if (d.properties.PRESENT00=="Yes") {
+                          return greenRainbow.colourAt(3)
+                        }
+                        else if (d.properties.PRESENT05=="Yes") {
+                          return greenRainbow.colourAt(4)
+                        }
+                        else if (d.properties.PRESENT08=="Yes") {
+                          return greenRainbow.colourAt(5)
+                        }
+                        else if (d.properties.PRESENT09=="Yes") {
+                          return greenRainbow.colourAt(6)
+                        }
+                        else if (d.properties.PRESENT10=="Yes") {
+                          return greenRainbow.colourAt(7)
+                        }
+                        else if (d.properties.PRESENT11=="Yes") {
+                          return greenRainbow.colourAt(8)
+                        }
+                        else if (d.properties.PRESENT12=="Yes") {
+                          return greenRainbow.colourAt(9)
+                        }
+                        else if (d.properties.PRESENT13=="Yes") {
+                          return greenRainbow.colourAt(10)
+                        }
+                        else if (d.properties.PRESENT14=="Yes") {
+                          return greenRainbow.colourAt(11)
+                        }
+                        else if (d.properties.PRESENT15=="Yes") {
+                          return greenRainbow.colourAt(12)
+                        }
+                        else if (d.properties.PRESENT16=="Yes") {
+                          return greenRainbow.colourAt(13)
+                        }
+                        else if (d.properties.PRESENT17=="Yes") {
+                          return greenRainbow.colourAt(14)
+                        }
+                        else if (d.properties.PRESENT18=="Yes") {
+                          return greenRainbow.colourAt(15)
+                        }
+                      })
+
+   function update() {
+     d3_features.attr("d", path);
+   };
+   dcMap.on("moveend", update);
+   update();
+
+  // fit the SVG element to leaflet's map layer
+  function reset() {
+    bounds = path.bounds(geoShape);
+    var topLeft = bounds[0]
+    var bottomRight = bounds[1];
+
+    svg.attr("width", bottomRight[0] - topLeft[0])
+       .attr("height", bottomRight[1] - topLeft[1])
+       .style("left", topLeft[0] + "px")
+       .style("top", topLeft[1] + "px");
+
+    g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+    // initialize the path data
+    d3_features.attr("d", path)
+               .style("fill-opacity", 0.7)
+               .attr('fill','blue');
+  };
+	dcMap.on("viewreset", reset);
 	reset();
+
 })
